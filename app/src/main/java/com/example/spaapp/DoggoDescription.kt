dd4fbe7a7cb7b3113349ab57cc8db4cd1a4e1102
maureen <maureen.gallois@epsi.fr>
2019-12-04 08:57:12 +0100

    Création d'une page description

---
+++ app/src/main/java/com/example/spaapp/DoggoDescription.kt
@@ -0,0 +1,12 @@
+package com.example.spaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DoggoDescription : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doggo_description)
+    }
+}
